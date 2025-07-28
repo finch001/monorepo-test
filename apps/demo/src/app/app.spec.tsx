c6875732428440eea243022972709b76f5ee6e34
finch <yan.zheng@lofty.com>
2025-07-28 14:15:17 +0800

    Remove greeting title test from App component tests

--- apps/demo/src/app/app.spec.tsx
+++ apps/demo/src/app/app.spec.tsx
@@ -13,14 +13,4 @@ describe('App', () => {
     expect(baseElement).toBeTruthy();
   });
 
-  it('should have a greeting as the title', () => {
-    const { getAllByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    expect(
-      getAllByText(new RegExp('Welcome @acme/demo', 'gi')).length > 0
-    ).toBeTruthy();
-  });
 });
